2a4629d52b22ce664024fed5898c5c0ac855a160
Diogo Silva <diogo6801@gmail.com>
2026-06-15 11:03:15 +0100

    rotas concluidas, alterações ao SQL

--- server.js
+++ server.js
@@ -52,15 +52,62 @@ app.get("/api/tarefas/:id", async (req,res) =>{
   res.status(200).json(tarefa[0])
 })
 
-  // POST
+  // POST --- É preciso criar o middleware validarTarefa
 app.post("/api/tarefas", validarTarefa, async (req,res) =>{
-  const {name, description, is_completed, priority, category, date_start, date_finish_pred} = req.body
+  const {name, description, status, priority, category, date_start, date_finish_pred} = req.body
   if (!name || !description || !category || !date_start || !date_finish_pred){
       return res.status(400).json({erro: "Preencher campos obrigatórios"})
   }
   const query ="INSERT INTO musica (name, description, is_completed, priority, category, date_start, date_finish_pred) VALUES (?,?,?,?,?,?,?)"
-  const [resposta] = await pool.execute(query, [name, description, is_completed, priority, category, date_start, date_finish_pred])
+  const [resposta] = await pool.execute(query, [name, description, status, priority, category, date_start, date_finish_pred])
   res.status(201).json({mensagem: "Tarefa criada com sucesso!"})
 })
 
+
+// PUT
+app.put("/api/tarefas/:id", validarTarefa, async (req,res) =>{
+    const id = Number(req.params.id)
+    const query = "SELECT * FROM tarefas WHERE id = ?"
+    const [tarefa] = await pool.execute(query, [id])
+    if (tarefa.length === 0){
+        res.status(404).json({mensagem: "Esta tarefa não existe!"})
+    }
+    const {name, description, status, priority, category, date_start, date_finish_pred} = req.body
+    if (!name || !description || !category || !date_start || !date_finish_pred){
+        return res.status(400).json({erro: "Preencher campos obrigatórios"})
+    }
+    const query2 = "UPDATE tarefa SET name = ?, description = ?, status = ?, priority = ?, category = ?, date_start = ?, date_finish_pred = ? WHERE id = ?"
+    const [resultado] = await pool.execute(query2, [name, description, status, priority, category, date_start, date_finish_pred, id])
+ 
+    res.status(200).json({mensagem: "Tarefa alterada com sucesso"})
+  })
+
+
+  // DELETE
+  app.delete("/api/tarefas/:id", async (req,res)=>{
+    const id = Number(req.params.id)
+    const query = "SELECT * FROM tarefas WHERE id = ?"
+    const [tarefa] = await pool.execute(query, [id])
+    if (tarefa.length === 0){
+        res.status(404).json({mensagem: "Esta tarefa não existe!"})
+    }
+    const query2 = "DELETE FROM tarefas WHERE id = ?"
+    await pool.execute(query2, [id])
+
+    res.status(200).json({mensagem: "Tarefa eliminada com sucesso"})
+})
+
+
+
+
+
+app.listen(PORT, async ()=>{
+    console.log(`O servidor está operacional na porta ${PORT}`)
+  try {
+    await pool.execute("SELECT 1")
+    console.log("Ligado à base de dados")
+  } catch (error) {
+    console.log("Erro na ligação ao servidor SQL")
+  }
+})
 
