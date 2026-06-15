abcd00d5248f0207bd4b7ff8caa2b217593a54a7
Diogo Silva <diogo6801@gmail.com>
2026-06-15 16:08:12 +0100

    validação

--- projeto_final DB.sql
+++ projeto_final DB.sql
@@ -4,11 +4,11 @@ USE to_do_list_db;
  
 CREATE TABLE IF NOT EXISTS tarefas (
   id          	    INT AUTO_INCREMENT PRIMARY KEY,
-  name        		  VARCHAR(255) NOT NULL,
+  name        		  VARCHAR(200) NOT NULL,
   description 		  TEXT NOT NULL,
-  status  	        VARCHAR(50) DEFAULT "iniciado",   --"iniciado", "concluido", "atrasado"
-  priority    		  TINYINT DEFAULT 2,        -- 1=low, 2=medium, 3=high
-  category    		  VARCHAR(50) NOT NULL,  -- "bem_estar", "lazer", "pessoal", "trabalho", "outro"
+  status  	        VARCHAR(50) DEFAULT "planeado",   -- "planeado", "iniciado", "concluido", "atrasado"
+  priority    		  TINYINT DEFAULT 2,        -- 1=baixa, 2=media, 3=alta
+  category    		  VARCHAR(50) NOT NULL,  -- "bem_estar", "lazer", "pessoal", "profissional", "outro"
   date_start  		  DATE,
   date_finish_pred	DATE NOT NULL,
   date_finish_real 	TIMESTAMP NULL,
